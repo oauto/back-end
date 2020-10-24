f839576d63d30d84153f0a5caa2ac538f017ce93
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-10-24 14:19:51 -0400

    initial commit after npm

---
+++ knexfile.js
@@ -0,0 +1,32 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {          
+      filename: './data/database.db3', // where to change the name of the database 
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+  },
+
+  production: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {          
+      filename: './data/database.db3', // where to change the name of the database 
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
+};
