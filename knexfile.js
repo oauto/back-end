4ab3ee991fc2ebc4a531ab498c3531af19d2dcff
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-10-28 16:24:59 -0400

    changed db directory

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {          
-      filename: './data/database.db3', // where to change the name of the database 
+      filename: './data/db.db3', // where to change the name of the database 
     },
     migrations: {
       directory: './data/migrations'
@@ -20,7 +20,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {          
-      filename: './data/database.db3', // where to change the name of the database 
+      filename: './data/db.db3', // where to change the name of the database 
     },
     migrations: {
       directory: './data/migrations'
